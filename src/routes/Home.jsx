ccb54b599940095e6d6082e6e12795fb505382cc
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2024-07-28 19:26:49 +0100

    adding sort by and filter functionality

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,12 +7,16 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
   const [currentUser, setCurrentUser] = useState();
   const [errorMessage, setErrorMessage] = useState('');
   const [eventData, setEventData] = useState();
-  const [filterTopic, setFilterTopic] = useState('');
-  const [filterTopicLabel, setFilterTopicLabel] = useState('');
-  const [filterArg, setFilterArg] = useState([]);
-  const [sortQuery, setSortQuery] = useState('created_at');
-  const [sortAscending, setSortAscending] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  // filter states
+  const [filterTopic, setFilterTopic] = useState('');
+  const [filterArg, setFilterArg] = useState([]);
+
+  // sort states
+  const [sortTopic, setSortTopic] = useState('created_at');
+  const [sortDirectionLabelOptions, setSortDirectionLabelOptions] = useState(['Newest post', 'Oldest post']);
+  const [sortAscending, setSortAscending] = useState(false);
 
   // filter arg arrays
   const [sportsArr, setSportsArr] = useState();
@@ -44,56 +48,55 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
 
   // get the events and set the filter options
   useEffect(() => {
-      async function getAllEvents() {
-        const {data, error} = await supabase
-        .from('events')
-        .select()
-        .order(sortQuery, { ascending: sortAscending })
+    async function getAllEvents() {
+      const {data, error} = await supabase
+      .from('events')
+      .select()
+      .order(sortTopic, { ascending: sortAscending })
 
-        if (error) {
-          setErrorMessage('There was an error recieving the events, please try again.');
-          return;
-        }
-
-        // logic to create filter arg arrays
-        // sports
-        const tempSportsArr = [];
-        data.map(row => {
-          if (!tempSportsArr.includes(row.sport)) tempSportsArr.push(row.sport); 
-        });
-        tempSportsArr.sort();
-        setSportsArr(tempSportsArr);
-
-        // location
-        const tempLocationArr = [];
-        data.map(row => {
-          if (!tempLocationArr.includes(row.location)) tempLocationArr.push(row.location);
-        });
-        tempLocationArr.sort();
-        setLocationArr(tempLocationArr);
-
-        setErrorMessage('');
-        setEventData(data);
+      if (error) {
+        setErrorMessage('There was an error recieving the events, please try again.');
+        return;
       }
+
+      // logic to create filter arg arrays
+      // sports
+      const tempSportsArr = [];
+      data.map(row => {
+        if (!tempSportsArr.includes(row.sport)) tempSportsArr.push(row.sport); 
+      });
+      tempSportsArr.sort();
+      setSportsArr(tempSportsArr);
+
+      // location
+      const tempLocationArr = [];
+      data.map(row => {
+        if (!tempLocationArr.includes(row.location)) tempLocationArr.push(row.location);
+      });
+      tempLocationArr.sort();
+      setLocationArr(tempLocationArr);
+
+      setErrorMessage('');
+      setEventData(data);
+    }
 
     getAllEvents();
   }, []);
 
-  // handle update for num arguments (arg is array)
-  async function handleUpdateResultsNum(topic, arg) {
+  // handle update for array filter arguments
+  async function handleUpdateResultsNum(topic, arg, sortTopic, ascendingBool) {
     setLoading(true);
     if (arg.length > 1) {
       const {data, error} = await supabase
       .from('events')
       .select()
-      .gte(topic, arg[1])
-      .lte(topic, arg[0])
-      .order(sortQuery, { ascending: sortAscending })
+      .gte(topic, arg[0])
+      .lte(topic, arg[1])
+      .order(sortTopic, { ascending: ascendingBool })
   
       if (error) {
         setErrorMessage(`There was an issue updating the results, please try again: ${error}`);
         setLoading(false);
-        console.log(error)
         return;
       }
   
@@ -105,12 +108,11 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
       .from('events')
       .select()
       .gte(topic, arg[0])
-      .order(sortQuery, { ascending: sortAscending })
+      .order(sortTopic, { ascending: ascendingBool })
   
       if (error) {
         setErrorMessage(`There was an issue updating the results, please try again: ${error}`);
         setLoading(false);
-        console.log(error)
         return;
       }
   
@@ -120,13 +122,13 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
     setLoading(false);
   }
   
-  // handle update for word arguments
-  async function handleUpdateResults(topic, arg) {
+  // handle update for string filter arguments
+  async function handleUpdateResults(topic, arg, sortTopic, ascendingBool) {
       const {data, error} = await supabase
       .from('events')
       .select()
       .eq(topic, arg)
-      .order(sortQuery, { ascending: sortAscending })
+      .order(sortTopic , { ascending: ascendingBool })
 
       if (error) {
         setErrorMessage(`There was an issue updating the results, please try again: ${error}`);
@@ -135,6 +137,22 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
 
       setErrorMessage('');
       setEventData(data);
+  }
+
+  // handle update when no filter desired
+  async function handleUpdateForNoFilter(sortTopic, ascendingBool) {
+    const {data, error} = await supabase
+    .from('events')
+    .select()
+    .order(sortTopic, { ascending: ascendingBool })
+
+    if (error) {
+      setErrorMessage('There was an error recieving the events, please try again.');
+      return;
+    }
+
+    setErrorMessage('');
+    setEventData(data);
   }
 
   if (isSessionValid === null || !currentUser || !eventData) {
@@ -157,49 +175,41 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
       <div>
         <h2>Filter</h2>
 
-        {/* sport */}
-        <input type="radio" id="sportRadio" name="filterRadios" onClick={() => {
-          setFilterTopic('sport');
-          setFilterTopicLabel('sport')
-          setFilterArg(sportsArr[0]);
-        }}/>
-        <label htmlFor="sportRadio">Sport</label>
+        {/* filter topics drop */}
+        <label htmlFor="fitlerTopicDrop">Filter By:</label>
+        <select id="filterTopicDrop" onChange={(e) => {
+          setFilterTopic(e.target.value);
+          switch(e.target.value) {
+            case '':
+              setFilterArg('');
+              break;
+            case 'sport':
+              setFilterArg(sportsArr[0]);
+              break;
+            case 'location':
+              setFilterArg(locationArr[0]);
+              break;
+            case 'price':
+              setFilterArg(['0.00', '0.00']);
+              break;
+            case 'available_spaces':
+              setFilterArg([0, 9]);
+              break;
+            case 'duration::text':
+              setFilterArg(['00:00:00', '00:29:59']);
+              break;
+          }
+        }}>
+          <option value="">None</option>
+          <option value="sport">Sport</option>
+          <option value="location">Location</option>
+          <option value="price">Price</option>
+          <option value="available_spaces">Available Spaces</option>
+          <option value="duration::text">Duration</option>
+        </select>
 
-        {/* price */}
-        <input type="radio" id="priceRadio" name="filterRadios" onClick={() => {
-          setFilterTopic('price');
-          setFilterTopicLabel('price');
-          setFilterArg(['0.00', '0.00']);
-        }}/>
-        <label htmlFor="priceRadio">Price</label>
-
-        {/* location */}
-        <input type="radio" id="locationRadio" name="filterRadios" onClick={() => {
-          setFilterTopic('location');
-          setFilterTopicLabel('location');
-          setFilterArg(locationArr[0]);
-        }}/>
-        <label htmlFor="locationRadio">Location</label>
-
-        {/* available spaces */}
-        <input type="radio" id="availableSpacesRadio" name="filterRadios" onClick={() => {
-          setFilterTopic('available_spaces');
-          setFilterTopicLabel('available spaces');
-          setFilterArg([0, 9]);
-        }} />
-        <label htmlFor="availableSpacesRadio">Available Spaces</label>
-
-        {/* duration */}
-        <input type="radio" id="durationRadio" name="filterRadios" onClick={() => {
-          setFilterTopic('duration::text');
-          setFilterTopicLabel('duration');
-          setFilterArg(['00:00:00', '00:29:59']);
-        }} />
-        <label htmlFor="durationRadio">Duration</label>
-
+        {/* filter arguments drop options (depending on filter topic) */}
         {filterTopic && <div>
-          <label htmlFor="filterArgs">Filter by {filterTopicLabel}</label>
-
           {/* sport filter arguments */}
           {filterTopic === 'sport' &&
             <select id="filterArgs" onChange={(e) => setFilterArg(e.target.value)}>
@@ -213,10 +223,10 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
 
           {/* location filter arguments */}
           {filterTopic === 'location' &&
-            <select id="filterArgs">
+            <select id="filterArgs" onChange={(e) => setFilterArg(e.target.value)}>
               {locationArr.map((location, index) => {
                 return(
-                  <option onClick={() => setFilterArg(location)} key={index}>{location}</option>
+                  <option value={location} key={index}>{location}</option>
                 )
               })}
             </select>
@@ -226,14 +236,14 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
           {filterTopic === 'price' &&
             <select id="filterArgs"
               onChange={(e) => {
-                const arrConversion = e.target.value.split(',');
+                const arrConversion = JSON.parse("[" + e.target.value + "]");
                 setFilterArg(arrConversion);
               }}>
-              <option value={'0.00, 0.00'}>FREE</option>
-              <option value={'0.01, 9.99'}>Less than £10</option>
-              <option value={'10.00, 19.99'}>£10 - £20</option>
-              <option value={'20.00, 49.99'}>£20 - £50</option>
-              <option value={'50.00'}>£50 or more</option>
+              <option value={[0, 0]}>FREE</option>
+              <option value={[0.01, 9.99]}>Less than £10</option>
+              <option value={[10, 19.99]}>£10 - £20</option>
+              <option value={[20, 49.99]}>£20 - £50</option>
+              <option value={[50]}>£50 or more</option>
             </select>
           }
 
@@ -257,8 +267,8 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
               setFilterArg(arrConversion);
             }}>
               <option value={'00:00:00, 00:29:59'}>Less than 30 mins</option>
-              <option value={'00:30:00, 00:59:59'}>30 mins - 1 hour</option>
-              <option value={'01:00:00, 01:59:59'}>1 hour - 2 hour</option>
+              <option value={'00:30:00, 00:59:59'}>Between 30 mins & 1 hour</option>
+              <option value={'01:00:00, 01:59:59'}>Between 1 & 2 hours</option>
               <option value={'02:00:00'}>2 hours or longer</option>
             </select>
           }
@@ -268,16 +278,52 @@ export default function Home({navigate, checkValidSession, isSessionValid, setIs
       {/* sort by */}
       <div>
         <h2>Sort</h2>
-        <input type="radio" />
+
+        {/* sort by topic drop */}
+        <label htmlFor="sortTopicDrop">Sort By: </label>
+        <select id="sortTopicDrop" onChange={(e) => {
+          setSortTopic(e.target.value);
+          switch(e.target.value) {
+            case 'created_at':
+              setSortDirectionLabelOptions(['Newest post', 'Oldest post']);
+              break;
+            case 'event_start':
+              setSortDirectionLabelOptions(['Latest event', 'Earliest event']);
+              break;
+            case 'price':
+              setSortDirectionLabelOptions(['Most Expensive event', 'Cheapest event']);
+              break;
+            case 'available_spaces':
+              setSortDirectionLabelOptions(['Most spaces', 'Least spaces']);
+              break;
+            case 'duration':
+              setSortDirectionLabelOptions(['Longest event', 'Shortest event']);
+              break;
+          }
+        }}>
+          <option value="created_at">Date Posted</option>
+          <option value="event_start">Event Time</option>
+          <option value="price">Price</option>
+          <option value="available_spaces">Available Spaces</option>
+          <option value="duration">Event Duration</option>
+        </select>
+
+        <select id="sortDirectionDrop" onChange={(e) => {
+          const formatValue = e.target.value === 'true' ? true : false;
+          setSortAscending(formatValue);
+        }}>
+          <option value={false}>{sortDirectionLabelOptions[0]} first</option>
+          <option value={true}>{sortDirectionLabelOptions[1]} first</option>
+        </select>
       </div>
 
+      {/* update results button */}
       <button onClick={() => {
-        if (Array.isArray(filterArg)) handleUpdateResultsNum(filterTopic, filterArg);
+        if (!filterTopic) handleUpdateForNoFilter(sortTopic, sortAscending);
+        else if (Array.isArray(filterArg)) handleUpdateResultsNum(filterTopic, filterArg);
         else handleUpdateResults(filterTopic, filterArg);
       }}>Update Results</button>
 
-      {/* {loading && <p>Loading events...</p>} */}
-      {/* {!loading && */}
       <div>
         {loading && <p>Loading events...</p>}
         {
