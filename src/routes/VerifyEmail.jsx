e20efd58eb65ebb9d4da1a88735d045802bfd129
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2024-08-02 21:40:03 +0100

    styling verify email page

--- src/routes/VerifyEmail.jsx
+++ src/routes/VerifyEmail.jsx
@@ -1,8 +1,9 @@
 export default function VerifyEmail () {
     return (
-        <div>
-            <h1>Thanks!</h1>
-            <p>You should shortly receive an email, click the link to verify your account</p>
+        <div className="verifyContainer">
+            <h1 className="verifyTitle titleFont">Acitvents</h1>
+            <h2 className="verifySubHeading">Thanks for signing up!</h2>
+            <p className="verifyText">You will shortly receive an email, click the link to verify your account!</p>
         </div>
     )
 }
